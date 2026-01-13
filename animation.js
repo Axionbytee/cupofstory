98b52a56e0f41de21852f13058cd61c536980a36
Rebienald Khei Carpio <rebienaldev@gmail.com>
2026-01-13 15:20:01 +0800

    Highlight and dev tools

--- animation.js
+++ animation.js
@@ -43,3 +43,13 @@
                 if (a.getAttribute("href").includes(current)) a.classList.add("active");
             });
         });
+
+        document.addEventListener('contextmenu', (e) => e.preventDefault());
+
+        document.addEventListener('keydown', (e) => {
+            if (e.key === 'F12' || 
+               (e.ctrlKey && e.shiftKey && ['I', 'J', 'C'].includes(e.key.toUpperCase())) ||
+               (e.ctrlKey && e.key.toUpperCase() === 'U')) {
+                e.preventDefault();
+            }
+        });
